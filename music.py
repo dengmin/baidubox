a3e8dc89f7980bef82230cfe9f61eed6ee5d49cb
dengmin <minhao123@gmail.com>
2013-12-27 22:55:10 +0800

    fixed

--- music.py
+++ music.py
@@ -47,6 +47,7 @@ songFormatUrl = 'http://yinyueyun.baidu.com/data/cloud/download'
 downloadUrl = 'http://yinyueyun.baidu.com/data/cloud/downloadsongfile'
 songInfoUrl = 'http://play.baidu.com/data/music/songinfo'
 
+
 class BaiduMusicBox(object):
 
     def __init__(self, username, password):
@@ -72,29 +73,28 @@ class BaiduMusicBox(object):
         urllib2.urlopen('http://play.baidu.com/')
 
     def request(self, url, method='GET', params={}, headers={}):
-    	params = urllib.urlencode(params)
-    	if method == 'POST':
-    		print params
-    		request = urllib2.Request(url, params)
-    	else:
-    		url = '%s?%s' %(url, params)
-    		request = urllib2.Request(url, None)
-
-    	if headers:
-    		for key,value in headers.items():
-    			request.add_header(key, value)
-    	try:
-    		response = urllib2.urlopen(request)
-    	except urllib2.HTTPError as e:
-    		print "Error code: ", e
-    	else:
-    		self.cjar.save()
-    		return response.read()
+        params = urllib.urlencode(params)
+        if method == 'POST':
+            request = urllib2.Request(url, params)
+        else:
+            url = '%s?%s' % (url, params)
+            request = urllib2.Request(url, None)
+        if headers:
+            for key, value in headers.items():
+                request.add_header(key, value)
+        try:
+            response = urllib2.urlopen(request)
+        except urllib2.HTTPError as e:
+            print "Error code: ", e
+        else:
+            self.cjar.save()
+            return response.read()
 
     def get_token(self):
         print 'request token...'
         response = self.request(url=apiUrl)
-        tokenreg = re.search("bdPass\.api\.params\.login_token='(?P<tokenVal>\w+)';", response)
+        tokenreg = re.search(
+            "bdPass\.api\.params\.login_token='(?P<tokenVal>\w+)';", response)
         if tokenreg:
             token = tokenreg.group("tokenVal")
             print 'get token', token
@@ -104,7 +104,8 @@ class BaiduMusicBox(object):
     def __signin(self):
         print 'login...'
         self.data["token"] = self.get_token()
-        resp = self.request(url=loginUrl,headers=HTTPHeader,params=self.data,method='POST')
+        resp = self.request(url=loginUrl, headers=HTTPHeader,
+                            params=self.data, method='POST')
         bdussreg = re.search("hao123Param=(?P<bdussVal>\w+)&", resp)
         if bdussreg:
             self.__bduss = bdussreg.group('bdussVal')
@@ -124,19 +125,20 @@ class BaiduMusicBox(object):
 
     def __login_cross_domain(self):
         params = {
-            'bdu' : self.__bduss,
-            't'   : int(time.time())
+            'bdu': self.__bduss,
+            't': int(time.time())
         }
         print 'cross domain '
         self.request(url=crossUrl, params=params)
 
     def get_playlist(self):
+        '''获取我的播放列表'''
         if not self.is_login:
             print 'no login.'
             self.__signin()
 
         print 'Request play list...'
-        resp = self.request(url=playlistUrl, params={'t':int(time.time())})
+        resp = self.request(url=playlistUrl, params={'t': int(time.time())})
         return json.loads(resp)
 
     def download_playlist(self):
@@ -149,10 +151,11 @@ class BaiduMusicBox(object):
                     self.get_song_info(songids)
 
     def get_list_detail(self, listid):
+        '''获取一个播放列表下面的所有歌曲的id'''
         params = {
-            'sid' : 1,
-            'playListId' : listid,
-            '_' : int(time.time())
+            'sid': 1,
+            'playListId': listid,
+            '_': int(time.time())
         }
         print 'get playlist detail for playListId', listid
         resp = self.request(url=playlistDetailUrl, params=params)
@@ -162,13 +165,39 @@ class BaiduMusicBox(object):
         return ids
 
     def get_song_info(self, songids):
-    	print 'get songinfo.'
-    	songids = ','.join(songids)
-    	response = self.request(url=songInfoUrl, method='POST',params={'songIds':songids})
-    	response = json.loads(response.decode('gbk'))
-    	songlist = response['data']['songList']
-    	for song in songlist:
-    		print song
+        '''
+        传入歌曲的id列表 返回这些歌曲的详细信息
+
+        albumId: 183666
+        albumName: "Beyond The Ultimate Story"
+        allRate: "24,64,128,192,256,320,flac"
+        area: 1
+        artistId: "1100"
+        artistName: "Beyond"
+        compress_status: 1
+        del_status: 0
+        distribution: "0000000000,0000000000,0000000000,0000000000,0000000000,0000000000,0000000000,0000000000,0000000000,0000000000"
+        fchar: "B"
+        queryId: "745994"
+        relateStatus: "0"
+        resourceType: "0"
+        songId: "745994"
+        songName: "午夜怨曲"
+        songPicBig: "http://a.hiphotos.baidu.com/ting/pic/item/9c16fdfaaf51f3de2ff8784596eef01f3a29794d.jpg"
+        songPicRadio: "http://a.hiphotos.baidu.com/ting/pic/item/91ef76c6a7efce1b0a6109ebad51f3deb48f654d.jpg"
+        songPicSmall: "http://c.hiphotos.baidu.com/ting/pic/item/b7fd5266d0160924434f633ad60735fae6cd34ad.jpg"
+
+        '''
+        print 'get songinfo.'
+        response = self.request(
+            url=songInfoUrl, method='POST', params={'songIds': ','.join(songids)})
+        response = json.loads(response.decode('gbk'))
+        songlist = response['data']['songList']
+        for song in songlist:
+            self.do_download(song)
+
+    def do_download(self, song):
+        print song
 
     def get_song_format(self, songid):
         params = {'songIds': songid}
@@ -177,13 +206,13 @@ class BaiduMusicBox(object):
         resp_data = json.loads(resp)
         formats = resp_data['data']['data']
         formats.pop('original')
-        for k,v in formats.items():
+        for k, v in formats.items():
             pp = {
-                'songIds' : v['songId'],
-                'rate' : v['rate'],
-                'format' : v['format']
+                'songIds': v['songId'],
+                'rate': v['rate'],
+                'format': v['format']
             }
-            filename = '%s_%s.%s' %(v['songId'],k, v['format'])
+            filename = '%s_%s.%s' % (v['songId'], k, v['format'])
             print 'download .', filename
             response = get_by_url(downloadUrl, pp)
             f = open(filename, 'wb+')
